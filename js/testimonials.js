e532773c264b501670eca87f570f5c3beb940b2d
Roberto Henríquez Perozo <rihp94@gmail.com>
2020-11-04 16:47:38 +0100

    fixing logos not displaying due to flex

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,14 +1,14 @@
 var currentSlide = 1;
 
-
 function showSlide(slideIndex) {
-  const slides = document.getElementsByClassName('carouselImgs');
+  const slides = document.getElementsByClassName('testimonialText');
   if (slideIndex > slides.length) { currentSlide = 1 }
   if (slideIndex < 1) { currentSlide = slides.length }
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
-  slides[currentSlide - 1].style.display = 'flex'
+  slides[currentSlide - 1].style.display = 'block'
+  console.log(slides, currentSlide)
 }
 
 
